0141dda6f24d4bcb9f72d562762d8a4caa09f55a
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2025-11-26 17:06:36 -0300

    server funcionando

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import { Remocao } from "./Remocao.js"
 
 const port = 3333
 
-const server = http.createServer( async (req, res) => {
+const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
     if (method === "GET" && url === "/remocao") {
@@ -13,12 +13,12 @@ const server = http.createServer( async (req, res) => {
             const listaRemocao = await Remocao.find()
 
             return res
-                .writeHead(200, {"Content-type": "application/json"})
+                .writeHead(200, { "Content-type": "application/json" })
                 .end(JSON.stringify(listaRemocao))
         } catch (error) {
             return res
-                .writeHead(400, {"Content-type": "application/json"})
-                .end(JSON.stringify({error: "Listagem de remoção não encontrado"}))
+                .writeHead(404, { "Content-type": "application/json" })
+                .end(JSON.stringify({ error: "Listagem de remoção não encontrado" }))
         }
 
     } else if (method === "POST" && url === "/remocao") {
@@ -38,16 +38,19 @@ const server = http.createServer( async (req, res) => {
             const newRemocao = await Remocao.create(body)
 
             return res
-                .writeHead()
-                .end()
+                .writeHead(201, { "Content-type": "application/json" })
+                .end(JSON.stringify(newRemocao))
         } catch (error) {
-            
+            return res
+                .writeHead(400, { "Content-type": "application/json" })
+                .end(JSON.stringify({ error: "Não foi possível criar novo atendimento para remoção" }))
         }
 
     } else {
-
+        return res
+            .writeHead(500, { "Content-type": "application/json" })
+            .end(JSON.stringify({ error: "Rota não encontrada" }))
     }
-
 })
 
 const startServer = async () => {
